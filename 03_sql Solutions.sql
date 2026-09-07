8f37240a82d15aeba7df48e56cd25abbcee2568e
Emmanuel Thomas <emmanuelthomas2044@gmail.com>
2026-09-07 21:14:01 +0530

    Create 03_sql Solutions.sql

---
+++ 03_sql Solutions.sql
@@ -0,0 +1,21 @@
+Q1 — Medium
+Find the total amount of orders for each customer. Display:
+CustomerID, CustomerName, TotalAmount
+
+Q2 — Medium
+Find the number of orders placed in each city. Display:
+City, OrderCount
+
+Q3 — Medium
+Find customers whose total order amount is greater than 50,000.
+
+Q4 — Medium-Hard
+Find the top 3 customers based on total order amount.
+
+Q5 — Hard
+Find the product category that generated the highest total revenue.
+
+Revenue = Price × Quantity
+
+Q6 — Hard
+Find customers who have placed at least one order but have never placed a Cancelled order.
